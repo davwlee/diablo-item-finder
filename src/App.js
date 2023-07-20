1087148012059793f1d9b415d5e057a13a7b96d0
Dave <davwleeders@gmail.com>
2023-07-20 01:38:46 -0500

    React tool that helps find Uniques in Diablo 4
    Simple react tool that has the Uniques in D4 listed in a drop down array and when one is selected, will give the user the item type, monsters that drop it and a list of dungeons

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ItemSelector from './ItemSelector.js';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Item Selector</h1>
+      <ItemSelector />
+    </div>
+  );
+};
+
+export default App;
