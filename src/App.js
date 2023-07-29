8679c795cdf115e1ea84fd2476794c5bf48c0a29
Dave <davwleeders@gmail.com>
2023-07-29 18:18:15 -0500

    Added Adsense Functionality
    Helps cover cost

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ItemSelector from './ItemSelector.js';
+import AdsComponent from './AdsComponent.js';
 
 const App = () => {
   return (
     <div>
       <h1>Unique Selector</h1>
       <ItemSelector />
+      <AdsComponent dataAdSlot='X7XXXXXX5X' />
     </div>
   );
 };
