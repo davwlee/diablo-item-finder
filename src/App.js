7ab2105e1139935db5da67ec9157f4830996f0b7
Dave <davwleeders@gmail.com>
2023-07-30 04:34:29 -0500

    Link updates
    There will be a couple link updates as I have to stagger these because I'm transferring a domain and setting up subdomains. Sorry!

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ const App = () => {
     <div>
       <nav>
         <a href="https://d4uniques.com"> Home </a>
-        <a href="https://d4uniques.com"> Unique Finder </a> 
-        <a href="https://d4uniques.com"> Rare Finder </a> 
+        <a href="https://uni.d4uniques.com/"> Unique Finder </a> 
+        <a href="https://rares.d4uniques.com"> Rare Finder </a> 
       </nav>
       <h1>Unique Selector</h1>
       <ItemSelector />
